3fbcc0513f31179de91e77c3635ea4fc802f67b5
ahmetdemirel <ahmetdemirel094@gmail.com>
2018-11-09 13:30:04 +0300

    browser

--- src/test/java/util/Browser.java
+++ src/test/java/util/Browser.java
@@ -41,7 +41,7 @@ public class Browser {
                 options.addArguments("start-maximized");
                 capabilities.setCapability(ChromeOptions.CAPABILITY, options);
                 capabilities.setCapability("key",key);
-                //BaseTest.setDriver(new RemoteWebDriver(hub, capabilities));
+                BaseTest.setDriver(new RemoteWebDriver(hub, capabilities));
 
 
         }
